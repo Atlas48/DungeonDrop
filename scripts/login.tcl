faccb0fde0862498f988c6a4eb2d806afb969a49
AtlasJan <AtlasJan@gmx.com>
2014-04-13 13:44:30 +0100

    Update login.tcl
    Made a HECKTON of additions

--- scripts/login.tcl
+++ scripts/login.tcl
@@ -1,12 +1,17 @@
-# This is heavaly work in progress
+# This is work in progress
 # This is meant to extend eggdrop's login feature 
 # This involves SQLite. A lot.
 package require sqlite 
 
 namespace eval login {
-#command used to lookup entries
-variable login::lookup ""
+#Command used to lookup entries
+variable login::lookup "!lookup"
 
+#Command used to set entries
+variable login::set "!set"
+
+#Command used to add an entry
+variable login::add "!add"
 }
    
    if {[catch {source scripts/login.tcl>} err} {
@@ -15,11 +20,9 @@ variable login::lookup ""
             putlog "login.tcl loaded without errors"
     }
 	
-	sqlite db1 ~/sql/database
 
-	
-        sqlite db1 ~/sql/database1.sqlite
-        db1 eval {CREATE TABLE IF NOT EXISTS db1.usersheet
+sqlite db1 ~/sql/database1.sqlite
+db1 eval {CREATE TABLE IF NOT EXISTS db1.usersheet
         (
         ID int(255)
         Adventurer varchar(255)
@@ -30,19 +33,20 @@ variable login::lookup ""
         DEX int(255)
         CON int(255)
         WIZ int(255)
-        CHA int(255)x
+        CHA int(255)
         )
         }
-	bind join * * joinup
+bind join * * joinup
 
        procname joinup {nick hand idx chan} {
-          if {db1 exists {SELECT $nick FROM Adventurer} == true} {
-	  puthelp "PRVMSG: Welcome back $nick"
+          if {db1 exists {SELECT * FROM Adventurer WHERE Adventurer=$nick} == true} {
+	  puthelp "PRVMSG $nick: Welcome back $nick"
 	  puthelp "$nick has joined!"
 	  chattr $nick +v chan
           } else {
-
-	 
+	 puthelp "PRVMSG $nick: Welcome, $nick. According to our Database, you haven't registered yet."
+	 puthelp "PRVMSG $nick: To register, type '/msg goruchwyliwr $login' "
+	 puthelp "PRVMSG $chan: We have a new member. $nick!"
 	 }   
 	
 
